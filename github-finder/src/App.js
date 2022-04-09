cc3b7ae13f5c413f4d4aa27c9766c9686458824f
Kyaw-Zin-Latt <kyawzinlat43021@gmail.com>
2022-04-09 14:17:50 +0630

    tailwindcss and daisyui setup

---
+++ github-finder/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-sky-400 font-bold underline font-ubuntu">
+        Hello world!
+      </h1>
+      <button className='btn glass'>
+        Click
+      </button>
+      <div class="mockup-code">
+        <pre data-prefix="$"><code>npm i daisyui</code></pre>
+      </div>
+    </div>
+  );
+}
+
+export default App;
